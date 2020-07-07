89adcc7ffac4986a2c18c2754a70f94b23a61e40
estherkang14 <estherkang14@gmail.com>
2020-07-07 14:50:37 -0400

    trying to add filter for pokemon card index

--- app/controllers/pokemon_cards_controller.rb
+++ app/controllers/pokemon_cards_controller.rb
@@ -6,4 +6,23 @@ class PokemonCardsController < ApplicationController
     def show
         @pokemon_card = PokemonCard.find(params[:id])
     end
+
+    def edit
+        @pokemon_card = PokemonCard.find(params[:id])
+    end
+
+    def update
+        @pokemon_card = PokemonCard.find(params[:id])
+        if @pokemon_card.update(card_params)
+            redirect_to pokemon_card_path(@pokemon_card.id)
+        else
+            render :edit
+        end 
+    end 
+
+    private
+
+    def card_params
+        params.require(:pokemon_card).permit(:title, :image_url, :type_ids)
+    end 
 end
