bccee42f5f7a5435dbaef6a26c7f0e6afa3e90ca
estherkang14 <estherkang14@gmail.com>
2020-07-07 12:42:06 -0400

    added create new user functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#login"
   post "/login", to: "sessions#process_login"
   get "/logout", to: "sessions#logout"
+  get "/create", to: "users#new"
+  post "/create", to: "users#create"
   # root "pokemon_cards#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
