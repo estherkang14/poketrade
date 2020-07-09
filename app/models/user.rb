1ef7842165f986b4e2d52b1531e9e65805c66621
estherkang14 <estherkang14@gmail.com>
2020-07-09 16:02:47 -0400

    fixed something

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
     has_many :user_collections
     has_many :pokemon_cards, through: :user_collections
     has_many :posts
+    validates :name, presence: true, uniqueness: true
+    validates :password, presence: true, length: { minimum: 4 } 
     has_secure_password
 end
  
