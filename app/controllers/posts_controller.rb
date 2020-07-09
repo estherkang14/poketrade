e6e1ac5b32541add83f401f8190deeff21dd3cfe
estherkang14 <estherkang14@gmail.com>
2020-07-09 12:57:02 -0400

    worked on form, new is not working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
     before_action :find_post, only: [:show, :edit, :update, :destroy]
     def index
-        @posts = Post.all.order("created_at: DESC")
+        @posts = Post.all
+        #.order("created_at: DESC")
     end
 
     def show
@@ -9,9 +10,16 @@ class PostsController < ApplicationController
     end 
 
     def new 
+        @post = Post.new
     end
 
     def create
+        @post = Post.new(post_params)
+        if @post.save 
+            redirect_to post_path(@post.id)
+        else 
+            render :new
+        end 
     end 
 
     def edit
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_190715) do
+ActiveRecord::Schema.define(version: 2020_07_09_161904) do
 
   create_table "pokemon_card_types", force: :cascade do |t|
     t.integer "pokemon_card_id"
@@ -22,6 +22,21 @@ ActiveRecord::Schema.define(version: 2020_07_07_190715) do
   create_table "pokemon_cards", force: :cascade do |t|
     t.string "title"
     t.string "image_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
+
+  create_table "trades", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "pokemon_card_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
